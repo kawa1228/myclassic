f4d4a70cd21853adf837c31bb952a435da7b4b32
kawa1228 <sxa.1105bo@gmail.com>
2020-04-04 22:34:50 +0900

    国テーブルのマイグレーションファイルを新規作成

---
+++ src/laravel/app/database/migrations/2020_04_04_125706_create_countries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->comment('日本名')->nullable();
+            $table->string('name_origin')->comment('現地名')->nullable();
+            $table->string('ioc_code')->comment('IOSコード')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
